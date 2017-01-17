8c4993570eeed664a3bc33784b1b1618d584c5ad
KOEM SEAN <koemseansr@gmail.com>
2017-01-18 01:30:24 +0700

    Contact Page Update Ready
    Please help to check this page

---
+++ application/views/pages/contact.php
@@ -0,0 +1,17 @@
+<div class="container">
+	<section>
+		<h2 class="thin">Contact Us</h2>
+		<p>
+			If you would like contact us, please complete the form below. Our team will review your feedback and aim to respond as soon as possible. Alternatively you may also like to contact an individual property using the Hotel Number directory. For customers in the USA please call 1 866 763 4553 for all Accommodation reservations.
+		</p>
+
+		<p>
+			If you have any questions about a booking that you have made on www.thistle.comor for any query regarding our website, please contact customer care at any of our hotels.
+		</p>
+
+		<p>
+			If you have stayed with us and would like to provide us with some feedback on your stay to help us improve our services click here.
+		</p>
+
+	</section>
+</div>
